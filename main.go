bdace8ba434299e5c95704f235865b3c7cbba9e7
sndnvaps <sndnvaps@gmail.com>
2017-03-11 11:03:56 +0800

    主界面: 使用 ui.MainWindow 来控制主界面
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- main.go
+++ main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/visualfc/goqt/ui"
-	"log"
 	"os"
-	"runtime"
 )
 
 func main() {
 	ui.RunEx(os.Args, func() {
-		w, err := NewMainForm()
-		if err != nil {
-			log.Fatalln(err)
-		}
+		w := NewMainWindow()
 		w.Show()
 	})
-}
-
-func version() {
-	info := fmt.Sprintf("GoQt Version %v \nQt Version %v\ngo verison %v %v/%v",
-		ui.Version(),
-		ui.QtVersion(),
-		runtime.Version(), runtime.GOOS, runtime.GOARCH)
-
-	lable := ui.NewLabel()
-	lable.SetText(info)
-
-	hbox := ui.NewHBoxLayout()
-	hbox.AddWidget(lable)
-
-	widget := ui.NewWidget()
-	widget.SetLayout(hbox)
-	widget.Show()
 }
--- mainform.go
+++ mainform.go
@@ -7,7 +7,12 @@ import (
 	"reflect"
 )
 
-type MainForm struct {
+type MainWindowForm struct {
+	*ui.QMainWindow
+	widget *MainForm //我们要加载 MainForm 到这里
+}
+
+type MainForm struct { //此为widget,
 	*ui.QWidget
 	btn1 *ui.QPushButton //
 	btn2 *ui.QPushButton
@@ -18,6 +23,41 @@ type MainForm struct {
 
 func IsValidDriver(v ui.Driver) bool {
 	return !reflect.ValueOf(v).IsNil()
+}
+
+func NewMainWindow() *MainWindowForm {
+	w := &MainWindowForm{}
+	w.QMainWindow = ui.NewMainWindow()
+	w.InstallEventFilter(w)
+
+	w.widget, _ = NewMainForm()
+	w.SetCentralWidget(w.widget)
+	w.createActions()
+
+	return w
+}
+
+func (w *MainWindowForm) createActions() {
+
+	aboutAct := ui.NewActionWithTextParent("&About", w)
+	aboutAct.SetStatusTip("Show the application's About box")
+	aboutAct.OnTriggered(func() {
+		ui.QMessageBoxAbout(w, "关于特斯拉线圈计算器",
+			w.Tr("特斯拉线圈计算器是使用 goqt 来编写的工具 "+
+				"用于计算制作特斯拉线圈各个过程中使用到的相关参数, "+
+				"目的是简化复杂的计算流程."))
+	})
+	aboutQtAct := ui.NewActionWithTextParent("About &Qt", w)
+	aboutQtAct.SetStatusTip("Show the Qt library's About box")
+	aboutQtAct.OnTriggered(func() { ui.QApplicationAboutQt() })
+
+	helpMenu := w.MenuBar().AddMenuWithTitle("&Help")
+	helpMenu.AddAction(aboutAct)
+	helpMenu.AddSeparator()
+	helpMenu.AddAction(aboutQtAct)
+
+	w.SetWindowTitle("特斯拉线圈计算器")
+	w.StatusBar().ShowMessage("Ready")
 }
 
 func NewMainForm() (*MainForm, error) {
@@ -67,7 +107,7 @@ func NewMainForm() (*MainForm, error) {
 	})
 
 	w.btn5.OnClicked(func() {
-		cc, err  := NewCoefficientForm()
+		cc, err := NewCoefficientForm()
 		if err != nil {
 			log.Fatalln(err)
 		}
