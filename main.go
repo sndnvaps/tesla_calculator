bdea2419230492ed2b62ab1b8179ae493240def2
sndnvaps <sndnvaps@gmail.com>
2019-04-06 00:06:01 +0800

    Add Application Icon

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 
 func main() {
 	ui.RunEx(os.Args, func() {
+		app := ui.Application()
+		app.SetOrganizationName("GoQt")
+		app.SetApplicationName("Application Example")
+		//func NewIconWithPixmap(pixmap *QPixmap) *QIcon {
+		//func NewIconWithFilename(fileName string) *QIcon {
+		//func (q *QApplication) SetWindowIcon(icon *QIcon) {
+		Icon := ui.NewPixmap()
+		IconData, _ := Asset("images/tesla.ico")
+		Icon.LoadFromData(IconData)
+		TC_Icon := ui.NewIconWithPixmap(Icon)
+		app.SetWindowIcon(TC_Icon)
 		w := NewMainWindow()
-		//w,err := NewMainForm()
-		//if err != nil {
-		//  log.Fatalln(err)
-		// }
 		w.Show()
 	})
 }
