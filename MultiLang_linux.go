cc6be81e5620fdd8ea849b3470a1954d2a8d2918
sndnvaps <sndnvaps@gmail.com>
2019-04-13 18:44:21 +0800

    [MultiLang] support Linux platform now

--- MultiLang_linux.go
+++ MultiLang_linux.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/Unknwon/i18n"
 	"github.com/sndnvaps/tesla_calculator/setting"
-	"strings"
 )
 
 type Controller struct {
@@ -19,13 +18,12 @@ func InitI18n() error {
 
 	langs := setting.Langs
 	for _, lang := range langs {
-		datapath := fmt.Sprintf("%s/conf/locale/locale_%s.ini", setting.GetCurrentDic(), strings.ToLower(lang))
+		datapath := fmt.Sprintf("%s/conf/locale/locale_%s.ini", setting.GetCurrentDic(), lang)
 
 		i18n.SetMessage(lang, datapath)
 	}
 	Lang = &Controller{
 		Locale: i18n.Locale{setting.DefLang}, //set default in conf/app.ini
 	}
-
 	return i18n.ReloadLangs(langs...)
 }
